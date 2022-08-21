ca9a37601fcce75ac22347729243023c4750e124
Javier Rojas <j.rojas1289@gmail.com>
2022-08-20 22:24:16 -0300

    Primer commit

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,63 @@
+import React, { createContext, CSSProperties } from 'react';
+import { useProducts } from '../hooks/useProducts';
+import { ProductContextProps, Product, onChangeArgs, InitialValues, ProductCardHandlers } from '../interfaces/interfaces';
+import { ProductImage } from './ProductImage';
+import { ProductTitle } from './ProductTitle';
+
+import styles from '../styles/styles.module.css';
+import { ProductButtons } from './ProductButtons';
+
+export interface Props {
+    product: Product;
+    // children?: ReactElement | ReactElement[];
+    children: ( arg: ProductCardHandlers )   => JSX.Element;
+    className?:string;
+    style?: CSSProperties;
+    onChange?: ( args: onChangeArgs ) => void;
+    value?: number;
+    initialValues?: InitialValues
+}
+
+
+export const productContext = createContext({} as ProductContextProps);
+
+const { Provider } = productContext;
+
+export const ProductCard = ({ children, product, className, style, onChange, value, initialValues }: Props ) => {
+
+    const { counter, increaseBy, maxCount, isMaxCountReached, reset } = useProducts({ onChange, product, value, initialValues }); 
+    
+    return (
+        <Provider value = {{
+            counter,
+            increaseBy,
+            product,
+            maxCount,
+        }}>
+            <div 
+                className={`${ styles.productCard } ${ className }`}
+                style = { style }
+            >
+                
+                { 
+                    children({
+                        count: counter,
+                        isMaxCountReached,
+                        maxCount:initialValues?.maxCount,
+                        product,
+
+                        increaseBy,
+                        reset
+                    })
+                } 
+                
+            </div>
+        </Provider>
+        
+     )
+}
+
+
+ProductCard.Title   = ProductTitle;
+ProductCard.Image   = ProductImage;
+ProductCard.Buttons = ProductButtons;
---
+++ src/components/ProductImage.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { CSSProperties, useContext } from "react";
+import { productContext } from "./ProductCard";
+
+import styles from '../styles/styles.module.css';
+import noImage from '../assets/no-image.jpg';
+
+export interface Props {
+    img?:string;
+    className?: string;
+    style?: CSSProperties;
+}
+
+export const ProductImage = ({ img = '', className, style }: Props ) => {
+
+    const { product } = useContext( productContext );
+
+    let imgToShow: string;
+
+    if( img ){
+        imgToShow = img;
+    }else if( product.img ){
+        imgToShow = product.img;
+    }else{
+        imgToShow = noImage;
+    }
+
+    return (
+        <img 
+            className={`${ styles.productImg } ${ className }`} 
+            src={ imgToShow } 
+            alt='Product image' 
+            style={ style }
+        />
+    );
+
+}
